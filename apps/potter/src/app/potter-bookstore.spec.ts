e9ab9a080f8c9e87f18645e262c5f59deb96f599
EricZhang1028 <yuchen2856@gmail.com>
2022-05-12 23:37:03 +0800

    red: buy one episode1 and one episode2

--- apps/potter/src/app/potter-bookstore.spec.ts
+++ apps/potter/src/app/potter-bookstore.spec.ts
@@ -1,3 +1,4 @@
+import { isExpressionFactoryMetadata } from '@angular/compiler/src/render3/r3_factory';
 import { TestBed } from '@angular/core/testing';
 import { PotterBookstore } from './potter-bookstore';
 
@@ -14,10 +15,15 @@ describe('PotterBookstore', () => {
   });
 
   // buy one 'Episode 1'
-  test('buyOneEsp1', () => {
+  test('buyOneEps1', () => {
     expect(pb.buy(1)).toBe(100);
   });
 
- 
+  // buy one 'Episode 1' and one 'Episode 2'
+  test('buyOneEps1AndOneEps2', () => {
+    let p1: number = pb.buy(1);
+    let p2: number = pb.buy(2);
+    expect(p1+p2).toBe(190);
+  })
 
 });
