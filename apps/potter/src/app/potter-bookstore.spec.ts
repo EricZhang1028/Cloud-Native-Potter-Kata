d6a0b62317fd5bacda744c3d88fd9dcca54529a2
EricZhang1028 <yuchen2856@gmail.com>
2022-05-12 23:30:09 +0800

    buy one episode1

--- apps/potter/src/app/potter-bookstore.spec.ts
+++ apps/potter/src/app/potter-bookstore.spec.ts
@@ -13,8 +13,11 @@ describe('PotterBookstore', () => {
     expect(new PotterBookstore()).toBeTruthy();
   });
 
-  test('buyOneBook', () => {
+  // buy one 'Episode 1'
+  test('buyOneEsp1', () => {
     expect(pb.buy(1)).toBe(100);
   });
+
+ 
 
 });
