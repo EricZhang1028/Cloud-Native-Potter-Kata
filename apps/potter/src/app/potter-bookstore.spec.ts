ce73b41b39ac995cb93270b6edb8cc904679da40
EricZhang1028 <yuchen2856@gmail.com>
2022-05-13 00:52:10 +0800

    red: buy one episode1, two episode2, one episode3, one episode4, one episode5

--- apps/potter/src/app/potter-bookstore.spec.ts
+++ apps/potter/src/app/potter-bookstore.spec.ts
@@ -25,7 +25,7 @@ describe('PotterBookstore', () => {
     pb.buy(0, 1);
     pb.buy(1, 1);
     expect(pb.price).toBe(190);
-  })
+  });
 
   /* buy one 'Episode 1'
   *      two 'Episode 2' */
@@ -33,7 +33,7 @@ describe('PotterBookstore', () => {
     pb.buy(0, 1);
     pb.buy(1, 2);
     expect(pb.price).toBe(290);
-  })
+  });
 
   /* buy one 'Episode 1'
    *     two 'Episode 2'
@@ -43,7 +43,7 @@ describe('PotterBookstore', () => {
     pb.buy(1, 2);
     pb.buy(2, 1);
     expect(pb.price).toBe(370);
-  })
+  });
 
   /* buy one 'Episode 1'
    *     two 'Episode 2'
@@ -55,5 +55,19 @@ describe('PotterBookstore', () => {
       pb.buy(2, 1);
       pb.buy(3, 1);
       expect(pb.price).toBe(420);
-    })
+    });
+
+  /* buy one 'Episode 1'
+   *     two 'Episode 2'
+   *     one 'Episode 3' 
+   *     one 'Episode 4' 
+   *     one 'Episode 5 */
+      test('buyOneEps1AndOneEps2', () => {
+        pb.buy(0, 1);
+        pb.buy(1, 2);
+        pb.buy(2, 1);
+        pb.buy(3, 1);
+        pb.buy(4, 1);
+        expect(pb.price).toBe(475);
+      });
 });
